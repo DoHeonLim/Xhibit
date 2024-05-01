6fc6c2b728fd670a8c5120577b1247c237214fdf
nbko <nbko@uchicago.edu>
2024-05-01 18:40:40 +0900

    추가 버튼 작동함

--- src/pages/UpdatePorfolio/index.js
+++ src/pages/UpdatePorfolio/index.js
@@ -1,6 +1,6 @@
 const portfolio = document.querySelector(".resume-content");
 
-// 포폴 섹션
+// 포폴 섹션 - 학력, 수상이력, 자격증, 플젝
 const portfolioSection = [
 	{ className: "education", title: "학력" },
 	{ className: "awards", title: "수상이력" },
@@ -8,7 +8,7 @@ const portfolioSection = [
 	{ className: "projects", title: "프로젝트" },
 ];
 
-// 수정 + delete 버튼
+// 수정, delete 버튼 한세트로 만들어 주는 함수
 const initEditBtns = () => {
 	const btnContainer = document.createElement("div");
 	btnContainer.className = "buttons";
@@ -40,82 +40,60 @@ const initEditBtns = () => {
 	return btnContainer;
 };
 
-function createDivide() {
+const createDivider = () => {
 	const span = document.createElement("span");
-	span.innerText = ` . `;
+	span.innerText = ".";
 	return span;
-}
+};
 
+// 각 연월일을 입력할 인풋 만들어주는 함수
+const createInput = (name, maxLength, placeholder) => {
+	const input = document.createElement("input");
+	input.name = name;
+	input.maxLength = maxLength;
+	input.placeholder = placeholder;
+	return input;
+};
+
+// 날짜 입력 받는 html 만드는 인풋 합수
 function createDateInput(section) {
-	const date = document.createElement("div");
-	date.className = "date";
+	const dateInput = document.createElement("div");
+	dateInput.className = "date";
+
+	const startYear = createInput("startYear", 4, "YYYY");
+	const startMonth = createInput("startMonth", 2, "MM");
+	const endYear = createInput("endYear", 4, "YYYY");
+	const endMonth = createInput("endMonth", 2, "MM");
+
+	const divider1 = createDivider();
+	const divider2 = createDivider();
+
+	const dash = document.createElement("span");
+	dash.innerText = "~";
 
 	if (section === "education" || section === "projects") {
-		const startYear = document.createElement("input");
-		startYear.name = "startYear";
-		startYear.maxLength = 4;
-		startYear.placeholder = "YYYY";
-
-		const divide = createDivide();
-
-		const startMonth = document.createElement("input");
-		startMonth.name = "startMonth";
-		startMonth.maxLength = 2;
-		startMonth.placeholder = "MM";
-
-		date.append(startYear);
-		date.append(divide);
-		date.append(startMonth);
-
-		const to = document.createElement("span");
-		to.innerText = ` ~ `;
-
-		const endYear = document.createElement("input");
-		endYear.name = "endYear";
-		endYear.maxLength = 4;
-		endYear.placeholder = "YYYY";
-
-		const divide2 = createDivide();
-
-		const endMonth = document.createElement("input");
-		endMonth.name = "startMonth";
-		endMonth.maxLength = 2;
-		endMonth.placeholder = "MM";
-
-		date.append(to);
-		date.append(endYear);
-		date.append(divide2);
-		date.append(endMonth);
+		dateInput.append(
+			startYear,
+			divider1,
+			startMonth,
+			dash,
+			endYear,
+			divider2,
+			endMonth
+		);
 	} else {
-		const year = document.createElement("input");
-		year.name = "startYear";
-		year.maxLength = 4;
-		year.placeholder = "YYYY";
-
-		const divide = createDivide();
-
-		const month = document.createElement("input");
-		month.name = "startMonth";
-		month.maxLength = 2;
-		month.placeholder = "MM";
-
-		const divide2 = createDivide();
-
 		const day = document.createElement("input");
 		day.name = "startMonth";
 		day.maxLength = 2;
 		day.placeholder = "DD";
 
-		date.append(year);
-		date.append(divide);
-		date.append(month);
-		date.append(divide2);
-		date.append(day);
+		dateInput.append(startYear, divider1, startMonth, divider2, day);
 	}
-	return date;
+
+	return dateInput;
 }
 
-// 학력, 상, 자격증, 플젝
+// 학력, 상, 자격증, 플젝 - 각 섹션의 인풋 폼을 만들어 주는 함수
 const initSectionForm = (section) => {
 	const sectionContainer = document.createElement("div");
 	sectionContainer.className = `portfolio-section ${section}`;
@@ -125,100 +103,113 @@ const initSectionForm = (section) => {
 
 	const editBtnContainer = initEditBtns();
 
-	let date = document.createElement("div");
-	date.className = "date";
+	let date = createDateInput(section);
 
-	// 학력: 학교명, 날짜
+	// 순서대로
+	// 학력: 학교명, 전공 및 학위, 날짜
 	// 수상이력, 자격증은 구조가 같음 - 날짜, 이름 + 기관
 	// 프로젝트는 이름,날짜, 링크, 플젝 소개
 	if (section === "education" || section === "projects") {
 		const name = document.createElement("input");
 		name.className = section === "education" ? "school-name" : "proj-name";
 		name.placeholder = section === "education" ? "학교명" : "프로젝트 이름";
-
-		const startDate = createDateInput(section);
-		date.appendChild(startDate);
-
 		sectionInput.appendChild(name);
+
 		if (section === "education") {
 			const major = document.createElement("input");
 			major.className = "major";
 			major.placeholder = "전공 및 학위 (ex. 경영학과 학사)";
 			sectionInput.appendChild(major);
+			sectionInput.appendChild(date);
 		}
 		if (section === "projects") {
+			sectionInput.appendChild(date);
+
 			const link = document.createElement("input");
 			link.placeholder = "프로젝트 링크";
+			sectionInput.appendChild(link);
+
 			const details = document.createElement("input");
 			details.placeholder = "프로젝트 소개";
-
-			sectionInput.appendChild(date);
-			sectionInput.appendChild(link);
 			sectionInput.appendChild(details);
 		}
 	} else {
-		const startDate = createDateInput(section);
-		date.appendChild(startDate);
 		sectionInput.appendChild(date);
 
 		const details = document.createElement("div");
 		details.className = `details ${section}`;
+
 		const name = document.createElement("input");
 		name.placeholder = section === "awards" ? "수상명" : "자격명";
+		details.appendChild(name);
+
 		const institution = document.createElement("input");
 		institution.placeholder = "발급 기관";
-		details.appendChild(name);
 		details.appendChild(institution);
+
 		sectionInput.appendChild(details);
 	}
 
-	section === "education" ? sectionInput.appendChild(date) : null;
 	sectionContainer.appendChild(sectionInput);
 	sectionContainer.appendChild(editBtnContainer);
 
 	return sectionContainer;
 };
 
-// 포폴 섹션 박스 + 제목 업데이트
-for (let i = 0; i < portfolioSection.length; i++) {
-	const eachSection = portfolioSection[i];
+const waitForElement = (selector) => {
+	return new Promise((resolve) => {
+		const element = document.querySelector(selector);
+		if (element) {
+			resolve(element);
+		} else {
+			const observer = new MutationObserver((mutations) => {
+				const element = document.querySelector(selector);
+				if (element) {
+					resolve(element);
+					observer.disconnect();
+				}
+			});
+			observer.observe(document.body, { childList: true, subtree: true });
+		}
+	});
+};
 
-	const newSection = document.createElement("div");
-	newSection.className = "section";
-	newSection.classList.add(eachSection.className);
+// 각 섹션 토대 만드는 함수 - 박스 + 제목
+const createSection = (sectionData) => {
+	const section = document.createElement("div");
+	section.className = "section";
+	section.classList.add(sectionData.className);
 
 	const header = document.createElement("div");
 	header.className = "title-bar";
 
 	const title = document.createElement("h3");
-	title.innerText = eachSection.title;
+	title.innerText = sectionData.title;
+	header.appendChild(title);
 
 	const addNewItemBtn = document.createElement("button");
 	addNewItemBtn.innerText = "+ 추가";
-
-	const sectionForm = initSectionForm(eachSection.className);
-
-	header.appendChild(title);
+	addNewItemBtn.addEventListener("click", async () => {
+		const newForm = initSectionForm(sectionData.className);
+		section.insertBefore(newForm, section.children[1]);
+	});
 	header.appendChild(addNewItemBtn);
-	newSection.appendChild(header);
-	newSection.appendChild(sectionForm);
-	portfolio.appendChild(newSection);
-}
+	section.appendChild(header);
 
-// const waitForElement = (selector) => {
-// 	return new Promise((resolve) => {
-// 		const element = document.querySelector(selector);
-// 		if (element) {
-// 			resolve(element);
-// 		} else {
-// 			const observer = new MutationObserver((mutations) => {
-// 				const element = document.querySelector(selector);
-// 				if (element) {
-// 					resolve(element);
-// 					observer.disconnect();
-// 				}
-// 			});
-// 			observer.observe(document.body, { childList: true, subtree: true });
-// 		}
-// 	});
-// };
+	const sectionForm = initSectionForm(sectionData.className);
+	section.appendChild(sectionForm);
+
+	return section;
+};
+
+// 포트폴리오 각 섹션 토대 업데이트 하는 함수
+const updatePortfolioSections = () => {
+	const portfolio = document.querySelector(".resume-content");
+
+	portfolioSection.forEach((section) => {
+		const newSection = createSection(section);
+		portfolio.appendChild(newSection);
+	});
+};
+
+updatePortfolioSections();
