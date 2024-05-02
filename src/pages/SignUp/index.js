9a75126db89c8d45304321218f88134524f4f418
[AI10] 임도헌 <ldh2233@gmail.com>
2024-05-02 14:55:09 +0900

    feat:signup validation update

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,3 +1,63 @@
+// 유저 네임
+const inputName = document.querySelector("#name");
+// 유저 이메일
+const inputEmail = document.querySelector("#email");
+
+// 1. 비밀번호 입력창 정보 가져오기
+const inputPassword = document.querySelector("#password"); // input#password
+// 2. 비밀번호 확인 입력창 정보 가져오기
+const inputPasswordRetype = document.querySelector("#password-retype"); // input#password-retype
+// 3. 실패 메시지 정보 가져오기 (비밀번호 불일치)
+const mismatchMessage = document.querySelector(".mismatch-message"); // div.mismatch-message.hide
+// 4. 실패 메시지 정보 가져오기 (8글자 이상, 영문, 숫자, 특수문자 미사용)
+const strongPasswordMessage = document.querySelector(".strongPassword-message"); // div.strongPassword-message.hide
+
+const submitButton = document.querySelector(".form-control-submit-button");
+// 전체 폼 입력 확인
+
+// 보안성 높이기위한 패스워드 체크
+function strongPassword(str) {
+  return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(
+    str
+  );
+}
+
+// 패스워드 재확인
+function isMatch(password1, password2) {
+  return password1 === password2;
+}
+
+// 패스워드 유효성 검사
+inputPassword.onkeyup = function () {
+  // console.log(inputPassword.value);
+  // 값 입력한 경우
+  if (inputPassword.value.length !== 0) {
+    if (strongPassword(inputPassword.value)) {
+      strongPasswordMessage.classList.add("hide"); //실패 메시지 숨김
+    } else {
+      strongPasswordMessage.classList.remove("hide"); //실패 메시지 보임
+    }
+  } else {
+    strongPasswordMessage.classList.add("hide");
+  }
+};
+
+// 패스워드 일치 검사
+inputPasswordRetype.onkeyup = function () {
+  // console.log(inputPasswordRetype.value);
+  if (inputPasswordRetype.value.length !== 0) {
+    if (isMatch(inputPassword.value, inputPasswordRetype.value)) {
+      mismatchMessage.classList.add("hide"); // 실패 메시지 숨김
+      submitButton.disabled = false;
+    } else {
+      mismatchMessage.classList.remove("hide"); // 실패 메시지 보임
+    }
+  } else {
+    mismatchMessage.classList.add("hide"); //실패 메시지 숨김
+  }
+};
+
+// 비밀번호 show / hide
 document.addEventListener("DOMContentLoaded", function () {
   const eyeIcon = document.querySelector(".form-group .password-eye");
   const passwordInput = document.querySelector(".form-control-input.password");
